43cf40fd739076df8d708cd3355b162147b3385d
Noel Thomson <noel.thomson@gmail.com>
2019-10-10 14:14:17 -0400

    refactor to blackjack

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-var gameOn, scores, roundScore, activePlayer, diceValue, diceDisplay;
-diceDisplay = document.querySelector('.dice');
+let gameOn;
+let scores;
+let roundScore;
+let activePlayer;
+let cardValue;
+let cardDisplay;
+let suits = ['C', 'D', 'H', 'S'];
+let faces = ['J', 'Q', 'K', 'N'];
+let drawnCards = [
+  playerOne = [],
+  playerTwo = [],
+];
 
 init();
 
@@ -8,14 +18,17 @@ function init() {
   scores = [0, 0];
   roundScore = 0;
   activePlayer = 0; // used to read score from scores array
+  drawnCards = [
+    playerOne = [],
+    playerTwo = [],
+  ];
 
-  diceDisplay.style.display = 'none';
+  cardDisplay = document.querySelector('.card');
+  cardDisplay.style.display = 'none';
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
-  document.getElementById('current-0').textContent = '0';
-  document.getElementById('current-1').textContent = '0';
-  document.getElementById('name-0').textContent = 'Player 1';
-  document.getElementById('name-1').textContent = 'Player 2';
+  document.getElementById('current-0').textContent = 'Player 1';
+  document.getElementById('current-1').textContent = 'Player 2';
   document.querySelector('.player-0-panel').classList.remove('winner');
   document.querySelector('.player-1-panel').classList.remove('winner');
   document.querySelector('.player-0-panel').classList.remove('active');
@@ -23,29 +36,74 @@ function init() {
   document.querySelector('.player-0-panel').classList.add('active');
 }
 
-// roll dice
-document.querySelector('.btn-roll').addEventListener('click', function() {
-  if(gameOn) {
-    diceValue = Math.floor(Math.random() * 6) + 1;
-    diceDisplay.src = 'dice-' + diceValue + '.png';
-    diceDisplay.style.display = 'block';
-    if(diceValue !== 1) {
-      roundScore += diceValue;
-      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+// draw card
+document.querySelector('.btn-draw').addEventListener('click', function () {
+  if (gameOn) {
+    cardValue = Math.floor(Math.random() * 10) + 1;
+    if (cardValue === 10) {
+      faceValue = Math.floor(Math.random() * 3) + 1;
+      suitValue = Math.floor(Math.random() * 3) + 1;
+      face = faces[faceValue];
+      suit = suits[suitValue];
+      cardDisplay.src = './cards/10' + face + suit + '.png';
+      cardDisplay.style.display = 'block';
     } else {
-      switchPlayer();
+      suitValue = Math.floor(Math.random() * 3) + 1;
+      suit = suits[suitValue];
+      cardDisplay.src = './cards/' + cardValue + suit + '.png';
+      cardDisplay.style.display = 'block';
+    }
+
+    if (cardValue === 1 && scores[activePlayer] < 11) {
+      cardValue = 11;
+    }
+
+    drawnCards[activePlayer].push(cardValue);
+    console.log('0: ' + drawnCards[0]);
+    console.log('1: ' + drawnCards[1]);
+    for (var i = 0; i < drawnCards[activePlayer].length; i++) {
+      if (drawnCards[activePlayer][i] === 11) { // if 11 was drawn
+        if ((scores[activePlayer] + cardValue) > 21) { // if
+          scores[activePlayer] -= 10; // subtract 10 from score
+        }
+      }
+    }
+
+    if (scores[activePlayer] + cardValue === 21) {
+      scores[activePlayer] += cardValue;
+      document.querySelector('#current-' + activePlayer).textContent = 'Winner';
+      document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+      gameOn = false;
+    } else if (scores[activePlayer] + cardValue > 21) {
+      scores[activePlayer] += cardValue;
+      document.querySelector('#current-' + activePlayer).textContent = 'Bust';
+      document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+      if (activePlayer === 0) {
+        document.querySelector('.player-0-panel').classList.remove('active');
+        document.querySelector('.player-1-panel').classList.add('active');
+        document.querySelector('#current-' + 1).textContent = 'Winner';
+      } else if (activePlayer === 1) {
+        document.querySelector('.player-1-panel').classList.remove('active');
+        document.querySelector('.player-0-panel').classList.add('active');
+        document.querySelector('#current-' + 0).textContent = 'Winner';
+      }
+
+      gameOn = false;
+    } else if (scores[activePlayer] + cardValue < 21) {
+      scores[activePlayer] += cardValue;
+      document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     }
   }
 });
 
 // hold score
-document.querySelector('.btn-hold').addEventListener('click', function() {
+document.querySelector('.btn-hold').addEventListener('click', function () {
   if (gameOn) {
     scores[activePlayer] += roundScore;
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-    if(scores[activePlayer] > 20) {
-      document.querySelector('#name-' + activePlayer).textContent = "Winner";
-      diceDisplay.style.display = 'none';
+    if (scores[activePlayer] === 21) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+      cardDisplay.style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
       gameOn = false;
@@ -57,16 +115,15 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
 // switch player
 function switchPlayer() {
-  roundScore = 0;
-  document.querySelector('#current-' + activePlayer).textContent = roundScore;
-  if(activePlayer === 0) {
+  if (activePlayer === 0) {
     activePlayer = 1;
   } else {
     activePlayer = 0;
   }
+
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
-  diceDisplay.style.display = 'none';
+  cardDisplay.style.display = 'none';
 }
 
 // new game
