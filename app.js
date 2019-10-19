8f43bf2dd8eea3dfa6701f50c99750d0e43a243f
Noel Thomson <noel.thomson@gmail.com>
2019-10-18 21:20:36 -0400

    ui, animation, SFX

--- app.js
+++ app.js
@@ -3,20 +3,29 @@ let scores;
 let activePlayer;
 let cardValue;
 let cardSrc;
-
-// let cardDisplay;
 let suits = ['C', 'D', 'H', 'S'];
 let faces = ['J', 'Q', 'K', 'N'];
 let drawnCard;
+let drawnCards;
 let holdCount;
+let drawCount;
 const cardDisplay = document.querySelector('.card');
 const playerOneCards = document.getElementById('drawn-cards-0');
 const playerTwoCards = document.getElementById('drawn-cards-1');
+const miniCards = document.getElementsByClassName('drawn-card');
 
-// myContainer.insertBefore(myElement, list.firstElementChild); <-- add to beginning
-// myContainer.removeChild(myElement); <-- remove specific element
-// const replaceMe = document.getElementById('replaceMe'); <-- get element
-// myContainer.replaceChild(replaceMe, newElement); <-- replace element with another
+const btnNewGame = document.querySelector('.btn-new');
+const btnDrawCard = document.querySelector('.btn-draw');
+
+const click = new Audio('./sounds/click.wav');
+const drawOne = new Audio('./sounds/draw-one.wav');
+const drawTwo = new Audio('./sounds/draw-two.wav');
+const bust = new Audio('./sounds/bust.wav');
+const winner = new Audio('./sounds/winner.wav');
+
+// myParent.insertBefore(myElement, myParent.firstElementChild); <-- add to beginning
+// myParent.removeChild(myElement); <-- remove specific element
+// myParent.replaceChild(replaceMe, newElement); <-- replace element with another
 
 init();
 
@@ -29,9 +38,11 @@ function init() {
     playerTwo = [],
   ];
   holdCount = 0;
+  drawCount = [
+    playerOne = [0],
+    playerTwo = [0],
+  ];
 
-  // cardDisplay = document.querySelector('.card');
-  // playerOneCards = document.getElementById('drawn-cards-0');
   cardDisplay.style.display = 'none';
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
@@ -42,14 +53,15 @@ function init() {
   document.querySelector('.player-0-panel').classList.remove('active');
   document.querySelector('.player-1-panel').classList.remove('active');
   document.querySelector('.player-0-panel').classList.add('active');
-  let miniCards = document.getElementsByClassName('drawn-card');
-  while (miniCards[0]) {
-    miniCards[0].parentNode.removeChild(miniCards[0]);
-  }
+  document.getElementById('current-0').classList.remove('winner-text');
+  document.getElementById('current-1').classList.remove('winner-text');
+  document.getElementById('current-0').classList.remove('draw-text');
+  document.getElementById('current-1').classList.remove('draw-text');
 
   while (miniCards[0]) {
     miniCards[0].parentNode.removeChild(miniCards[0]);
   }
+
 }
 
 // draw card
@@ -57,6 +69,9 @@ document.querySelector('.btn-draw').addEventListener('click', function () {
   if (gameOn) {
     holdCount = 0;
     cardValue = Math.floor(Math.random() * 10) + 1;
+    drawCount[activePlayer]++; // implement two cards
+    playDrawOne();
+
     if (cardValue === 10) {
       faceValue = Math.floor(Math.random() * 3) + 1;
       suitValue = Math.floor(Math.random() * 3) + 1;
@@ -73,7 +88,6 @@ document.querySelector('.btn-draw').addEventListener('click', function () {
     }
 
     drawnCard = document.createElement('img');
-    playerOneCards.appendChild(drawnCard);
 
     if (activePlayer === 0) {
       playerOneCards.appendChild(drawnCard);
@@ -84,33 +98,40 @@ document.querySelector('.btn-draw').addEventListener('click', function () {
     drawnCard.src = cardSrc;
     drawnCard.classList.add('drawn-card');
 
-    // myContainer.appendChild(myElement); <-- append element
-    // const images = document.getElementByClassName('myClass'); <-- obtain els
-    // while(images[0]) {
-    //     images[0].myContainer.removeChild(images[0]);
-    // }​
-
     if (cardValue === 1 && scores[activePlayer] < 11) {
       cardValue = 11;
     }
 
     drawnCards[activePlayer].push(cardValue); // push drawn card into arr
+
+    // convert ace value from 11 to 1
+    let index = drawnCards[activePlayer].indexOf(11);
+    if (scores[activePlayer] + cardValue > 21 && index !== -1) {
+      drawnCards[activePlayer][index] = 1; // turn 11 to 1
+      let sum = 0;
+      for (var i = 0; i < (drawnCards[activePlayer].length - 1); i++) {
+        sum += drawnCards[activePlayer][i];
+      }
+
+      scores[activePlayer] = sum; // calc score minus cardValue
+    }
+
     console.log('0: ' + drawnCards[0]);
     console.log('1: ' + drawnCards[1]);
-    for (var i = 0; i < drawnCards[activePlayer].length; i++) {
-      if (drawnCards[activePlayer][i] === 11) { // if 11 was drawn
-        if ((scores[activePlayer] + cardValue) > 21) { // and new score would be bust
-          scores[activePlayer] -= 10; // make 11 into 1 (subtract 10 from score)
-        }
-      }
-    }
 
     if (scores[activePlayer] + cardValue === 21) {
       scores[activePlayer] += cardValue;
       document.querySelector('#current-' + activePlayer).textContent = 'Winner';
       document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
       gameOn = false;
+      playWinner();
+      document.querySelector('#current-' + activePlayer).classList.add('winner-text');
     } else if (scores[activePlayer] + cardValue > 21) {
+      playBust();
+      setTimeout(function () {
+        playWinner();
+      }, 500);
+
       scores[activePlayer] += cardValue;
       document.querySelector('#current-' + activePlayer).textContent = 'Bust';
       document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
@@ -118,10 +139,12 @@ document.querySelector('.btn-draw').addEventListener('click', function () {
         document.querySelector('.player-0-panel').classList.remove('active');
         document.querySelector('.player-1-panel').classList.add('active');
         document.querySelector('#current-' + 1).textContent = 'Winner';
+        document.querySelector('#current-' + 1).classList.add('winner-text');
       } else if (activePlayer === 1) {
         document.querySelector('.player-1-panel').classList.remove('active');
         document.querySelector('.player-0-panel').classList.add('active');
         document.querySelector('#current-' + 0).textContent = 'Winner';
+        document.querySelector('#current-' + 0).classList.add('winner-text');
       }
 
       gameOn = false;
@@ -135,20 +158,33 @@ document.querySelector('.btn-draw').addEventListener('click', function () {
 // hold score
 document.querySelector('.btn-hold').addEventListener('click', function () {
   if (gameOn) {
+    if (drawCount[activePlayer] < 2) {
+      playBust();
+      return;
+    }
+
     holdCount++;
+    playClick();
     if (holdCount > 2 && (scores[0] > 0 && scores[1] > 0)) {
       if (scores[0] > scores[1]) {
+        playWinner();
         document.querySelector('#current-' + 0).textContent = 'Winner';
         document.querySelector('.player-1-panel').classList.remove('active');
         document.querySelector('.player-0-panel').classList.add('active');
+        document.querySelector('#current-' + 0).classList.add('winner-text');
       } else if (scores[1] > scores[0]) {
+        playWinner();
         document.querySelector('#current-' + 1).textContent = 'Winner';
         document.querySelector('.player-0-panel').classList.remove('active');
         document.querySelector('.player-1-panel').classList.add('active');
+        document.querySelector('#current-' + 1).classList.add('winner-text');
       } else if (scores[1] = scores[0]) {
+        playWinner();
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
         document.querySelector('#current-' + 0).textContent = 'Draw';
         document.querySelector('#current-' + 1).textContent = 'Draw';
+        document.querySelector('#current-' + 0).classList.add('draw-text');
+        document.querySelector('#current-' + 1).classList.add('draw-text');
       }
 
       gameOn = false;
@@ -173,4 +209,25 @@ function switchPlayer() {
 }
 
 // new game
-document.querySelector('.btn-new').addEventListener('click', init);
+btnNewGame.addEventListener('click', init);
+btnNewGame.addEventListener('click', playClick);
+
+function playClick() {
+  click.play();
+}
+
+function playBust() {
+  bust.play();
+}
+
+function playDrawOne() {
+  drawOne.play();
+}
+
+function playDrawTwo() {
+  drawTwo.play();
+}
+
+function playWinner() {
+  winner.play();
+}
